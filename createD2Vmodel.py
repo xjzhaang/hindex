bb9607b16f6d6d25830d6bde25e842c1520846d3
Diego Gomez <56383012+diego1401@users.noreply.github.com>
2021-12-08 16:45:56 +0100

    Update createD2Vmodel.py
    Fixed

--- createD2Vmodel.py
+++ createD2Vmodel.py
@@ -6,13 +6,9 @@ import pandas as pd
 from scipy.spatial.distance import euclidean, pdist, squareform
 import networkx as nx
 import scipy as sp
-import os
 
-if not os.path.exists("AbstractEmb"):
-    os.makedirs("AbstractEmb")
-
-def create_Embedding(input_file,n_dim,window_size,min_count,epochs,model_name):
-    model = Doc2Vec(min_count=min_count,vector_size=n_dim,window=window_size)
+def create_Embedding(input_file,n_dim,window_size,min_count,epochs,model_name,algtype):
+    model = Doc2Vec(min_count=min_count,vector_size=n_dim,window=window_size,dm=algtype)
     t = TaggedLineDocument(input_file)
     model.build_vocab(t)
     model.train(t,total_examples=model.corpus_count,epochs=epochs)
@@ -20,13 +16,25 @@ def create_Embedding(input_file,n_dim,window_size,min_count,epochs,model_name):
 
 
 if __name__ == '__main__':
-
-    n_dim = 100
+    model_name = "d2v_model_PV-DBOW
+    alg_type = 0
+    n_dim = 50
     window_size=5
     input_file = "sentences_line.txt"
     min_count = 2
-    epochs = 5
-    create_Embedding(input_file,n_dim,window_size,min_count,epochs,model_name)
+    epochs = 10
+    print("Creating",model_name)
+    create_Embedding(input_file,n_dim,window_size,min_count,epochs,model_name,algtype)
+    print("Model saved")
+    model_name = "d2v_model_PV-DM"
+    alg_type = 1
+    n_dim = 50
+    window_size=5
+    input_file = "sentences_line.txt"
+    min_count = 2
+    epochs = 10
+    print("Creating",model_name)
+    create_Embedding(input_file,n_dim,window_size,min_count,epochs,model_name,algtype)
     print("Model saved")
 
     
